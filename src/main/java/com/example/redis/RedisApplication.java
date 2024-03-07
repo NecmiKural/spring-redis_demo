fa05d27bbedcd54b98ce2b94205897cc957444d4
neo <necmibkural@gmail.com>
2024-03-07 15:37:07 +0300

    CRUD operations with redis implemented

--- src/main/java/com/example/redis/RedisApplication.java
+++ src/main/java/com/example/redis/RedisApplication.java
@@ -1,14 +1,41 @@
 package com.example.redis;
 
-
+import com.example.redis.entity.Product;
+import com.example.redis.repository.ProductDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @SpringBootApplication
-
+@RestController
+@RequestMapping("/product")
 public class RedisApplication {
 
+    @Autowired
+    private ProductDao dao;
+
+    // we already define product above so no need another path
+    @PostMapping
+    public Product save(@RequestBody Product product) {
+        return dao.save(product);
+    }
+
+    @GetMapping
+    public List<Product> fetchAll() {
+        return dao.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findProductById(@PathVariable int id) {
+        return dao.findProductById(id);
+    }
+
+    public String removeProduct(@PathVariable int id) {
+        return dao.deleteProduct(id);
+    }
 
     public static void main(String[] args) {
 
---
+++ src/main/java/com/example/redis/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.example.redis.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+// TODO: dikkat buraya
+@EnableRedisRepositories(basePackages = "com.example.redis.repository")
+public class RedisConfig {
+
+    // TODO: be sure about that, docker
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("localhost", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    //we need to access redis from  our app
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+}
